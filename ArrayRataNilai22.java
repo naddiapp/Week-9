7891bf341ca6142ed328976876d97770d81c21cd
naddiapp <nadiapramudita43@gmail.com>
2023-11-02 23:30:08 +0700

    ArrayRataNilai22.java

--- ArrayRataNilai22.java
+++ ArrayRataNilai22.java
@@ -3,17 +3,37 @@ public class ArrayRataNilai22 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int[] nilaiMhs= new int[10];
-        double total= 0;
-        double rata2;
+        int tdkLulus= 0;
+        double total1= 0, total2= 0, rata2Lulus, rata2TdkLulus;
+
+        System.out.println("Masukkan jumlah mahasiswa: ");
+        int jmlMhs= sc.nextInt();
 
         for (int i= 0; i<nilaiMhs.length; i++) {
             System.out.print("Masukkan nilai mahasiswa ke-"+(i+1)+" : ");
             nilaiMhs[i] = sc.nextInt();
         }
         for (int i= 0; i<nilaiMhs.length; i++) {
-            total += nilaiMhs[i];
+            if (nilaiMhs[i] > 70) {
+                total1 += nilaiMhs[i];
+            }
+        }    
+        for (int i= 0; i<jmlMhs; i++) {
+            if (nilaiMhs[i] < 70) {
+            total2 += nilaiMhs[i];
+            tdkLulus++;
+            }
         }
-        rata2 = total/nilaiMhs.length;
-        System.out.println("Rata-rata nilai = "+rata2);
+        int totalLulus = 0;
+        for (int i= 0; i<nilaiMhs.length; i++) {
+            if (nilaiMhs[i]>70) {
+                totalLulus++;
+            }
+        rata2Lulus = total1/totalLulus;
+        rata2TdkLulus = total2/tdkLulus;
+        System.out.println("Rata-rata nilai lulus = "+rata2Lulus);
+        System.out.println("Rata-rata nilai tidak lulus = "+rata2TdkLulus);
+        System.out.println("Banyaknya mahasiswa yang lulus: "+totalLulus);
+        }
     }
 }
--- BubbleSortExample22.java
+++ BubbleSortExample22.java
@@ -4,8 +4,8 @@ public class BubbleSortExample22 {
         int temp= 0;
 
         for (int i=0; i<intData.length; i++) {
-            for (int j=1; j<intData.length-i; j++) {
-                if (intData[j-1] > intData[j]) {
+            for (int j=1; j>intData.length-i; j++) {
+                if (intData[j-1] < intData[j]) { 
                     //swap elemen
                     temp = intData[j];
                     intData[j] = intData[j-1];
