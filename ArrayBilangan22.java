791ac99be18dadee782c97be20c0dbd266e6f4d2
naddiapp <nadiapramudita43@gmail.com>
2023-11-02 21:14:21 +0700

    ArrayBilangan22.java

--- ArrayBilangan22.java
+++ ArrayBilangan22.java
@@ -2,16 +2,11 @@ import java.util.Scanner;
 public class ArrayBilangan22 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int[] bil = new int[4];
-        bil[0] = 5;
-        bil[1] = 13;
-        bil[2] = -7;
-        bil[3] = 17;
+        int[] bil = {5, 12867, 7, 0}; // Inisialisasi nilai array secara langsung
 
-        System.out.println(bil[0]);
-        System.out.println(bil[1]);
-        System.out.println(bil[2]);
-        System.out.println(bil[3]);
-        
+        for (int i= 0; i<=4; i++) {
+            System.out.println(bil[i]);
+        }
+
     }
 }
